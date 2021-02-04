bfa57a2e158e11d79a497df01006e96637cbd96a
Conrad Seto <conradseto@gmail.com>
2021-02-04 14:28:21 -0800

    feat: Add aria-label to brand link

--- themes/aleph-nought/resources/views/partials/header.blade.php
+++ themes/aleph-nought/resources/views/partials/header.blade.php
@@ -8,7 +8,9 @@
     <button class="siteHeader_Button mobileMenuToggle">
       Hamburger here
     </button>
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+  <a class="brand" href="{{ home_url('/') }}" aria-label="{{ get_bloginfo('name', 'display') }}">
+    {{ get_bloginfo('name', 'display') }}
+  </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
